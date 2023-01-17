7956ec21cbf15b4059143141cea2af3d7e0cf31e
Пользователь <polzovatelskijpolzovatel@protonmail.com>
2023-01-17 19:54:58 +0300

    Уфф

---
+++ public/site.js
@@ -0,0 +1,21 @@
+let myBtn = document.getElementById('add-btn')
+myBtn.addEventListener('click', addItem)
+
+document.addEventListener('keydown', entItem)
+
+function addItem(data) {
+    let myInput = document.getElementById('shooter')
+    let myList = document.getElementById('todo-list')
+
+    if (myInput.value !== '' && myInput.value !== null) {
+        let newItem = document.createElement('li')
+        newItem.classList.add('list-group-item');
+        newItem.innerText = myInput.value
+        myList.appendChild(newItem);
+    }
+}
+
+function entItem(inp) {
+    if (inp.key == 'Enter')
+        addItem()
+}
